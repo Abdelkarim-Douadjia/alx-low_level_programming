69be0de5f003679e2f3c99cd02560bdaf45e00b3
AbdelkarimDouadjia <dzkarim748@gmail.com>
2023-10-16 23:47:58 +0100

    Solve the TASK-2: 2.This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - length of a string
+ * @s: the str var.
+ *
+ * Return: The length of str
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
