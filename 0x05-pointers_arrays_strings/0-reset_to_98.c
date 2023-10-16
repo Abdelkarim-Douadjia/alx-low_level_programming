5a46790bda1638ac3951703da9cf3f894951c82a
AbdelkarimDouadjia <dzkarim748@gmail.com>
2023-10-16 23:20:03 +0100

    Solve the TASK-0: 0.98 Bettery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes pointer to int and set it value to 98
+ * @n: the pointer to integer
+ *
+ * Return: Nothing
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
