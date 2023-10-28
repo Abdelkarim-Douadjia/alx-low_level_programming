d85c0689f2ce467907a85893c94a4fa0ae08f752
AbdelkarimDouadjia <dzkarim748@gmail.com>
2023-10-28 01:31:51 +0100

    [0x0A-argc_argv]: Solve the TASK-1: 1. Silence is argument carried out by other means

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print number of arguments passed.
+ * @argc: number of commandline arguments.
+ * @argv: pointer to an array of command line arguments.
+ * Return: 0 if success, otherwise non zero failed.
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d", argc - 1);
+	return (0);
+}
